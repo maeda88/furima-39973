b2b3d7576db934fda289a681f524bd25732fb93b
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-01-12 16:19:30 +0900

    userテーブルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false)}
+    password_confirmation {password}
+    family_name           {'佐藤'}
+    first_name            {'次郎'}
+    family_name_kana      {'サトウ'}
+    first_name_kana       {'ジロウ'}
+    birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
+  end
+end
+
