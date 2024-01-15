4cfd582ea3bd30eb9efa0a54689e080c3034f934
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-01-15 10:07:52 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :trade_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :price, presence: true
+
 end
