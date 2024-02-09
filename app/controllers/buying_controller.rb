14c905404f2efaa01e104b1a25a7c52f7251cbce
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-02-09 15:39:52 +0900

    コントローラーの設定

--- app/controllers/buying_controller.rb
+++ app/controllers/buying_controller.rb
@@ -1,8 +1,22 @@
 class BuyingController < ApplicationController
   before_action :authenticate_user!
   def index
+    @buying_address = BuyingAddress.new
   end
 
   def create
+    @buying_address = BuyingAddress.new(buying_params)
+    if @buying_address.valid?
+      @buying_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def buying_params
+    params.require(:buying_address).permit(:item_id, :buying_id, :postcode, :prefecture, :municipality, :street_address, :building_name, :phonenumber).merge(user_id: current_user.id)
   end
 end
