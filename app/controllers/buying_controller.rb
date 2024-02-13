47cfbb0fefedbf03d01c28739ffb30571244f3b0
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-02-13 15:10:46 +0900

    商品　購入可不可での分岐

--- app/controllers/buying_controller.rb
+++ app/controllers/buying_controller.rb
@@ -1,5 +1,7 @@
 class BuyingController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_item_availability
+
   def index
     @item = Item.find(params[:item_id])
     @buying_address = BuyingAddress.new
@@ -19,5 +21,12 @@ class BuyingController < ApplicationController
 
   def buying_params
     params.require(:buying_address).permit(:postcode, :prefecture_id, :municipality, :street_address, :building_name, :phonenumber).merge(user_id: current_user.id, item_id: params[:item_id] )
+  end
+
+  def check_item_availability
+    @item = Item.find_by(id: params[:item_id])
+    if @item.buying.present?
+      redirect_to root_path
+    end
   end
 end
